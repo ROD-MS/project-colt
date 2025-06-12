f3f344d696e6f3ad923f3bb7c3c815ca5bacae37
ROD-MS <rodrigomoraesdasilva26@gmail.com>
2025-06-12 17:21:38 -0300

    alterando colisao player e add barra de vida visual

--- scripts/components/health_component.gd
+++ scripts/components/health_component.gd
@@ -3,13 +3,19 @@ extends Node3D
 class_name HealthComponent
 
 @export var MAX_HEALTH: int = 10
+@export var health_bar: ProgressBar
 var health: float
 
 func _ready():
 	health = MAX_HEALTH
+	if health_bar:
+		health_bar.max_value = MAX_HEALTH
+		health_bar.value = health
 	
 func damage(attack: Attack):
 	health -= attack.damage
+	if health_bar and health >= 0:
+		health_bar.value = health
 	print (health)
 	
 	if health <= 0:
