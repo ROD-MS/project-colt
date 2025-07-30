b5cf8a497014a46108494ea18845e82f3739e043
NamoeL19 <manumachadog@gmail.com>
2025-07-30 14:15:48 -0300

    Video Intro Satoware

---
+++ scripts/video_intro.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var video = $VideoStreamPlayer
+
+func _ready() -> void:
+	await video.finished	
+	print("Me ve dois alfajor por favor muchacho")
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
