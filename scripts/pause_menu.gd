a38dfd9125fb4af6c379b5fb7885d1a9b70f3fe4
ROD-MS <rodrigomoraesdasilva26@gmail.com>
2025-08-18 00:19:04 -0300

    luzes

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+
+
+func _on_resume_pressed() -> void:
+	pass # Replace with function body.
+	
+
+func _on_menu_pressed() -> void:
+	pass # Replace with function body.
+	
+
+func _on_configuration_pressed() -> void:
+	pass # Replace with function body.
+	
+	
+	
+func _on_quit_pressed() -> void:
+	get_tree().quit()
